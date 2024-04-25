af06497a378564bf236165b248b47362b7783eaf
elqnlsa <lectiodivina05@gmail.com>
2024-04-25 19:42:54 +0700

    Pemlan Pengayaan

---
+++ kasirVin/Main.java
@@ -0,0 +1,7 @@
+package kasirVin;
+public class Main {
+    public static void main(String[] args) {
+        Kasir kasir = new Kasir();
+        kasir.jalankan();
+    }
+}
---
+++ kasirVin/Pelanggan.java
@@ -0,0 +1,18 @@
+package kasirVin;
+
+
+public class Pelanggan {
+    private String nama;
+
+    public Pelanggan(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
